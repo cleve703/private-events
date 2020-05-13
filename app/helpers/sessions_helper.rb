3562c06d9ef5294b500c29d18e13703181d0c7bd
jay <cleve703@gmail.com>
2020-05-13 09:45:34 -0400

    improved sign-in and sign-out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,4 +8,12 @@ module  SessionsHelper
     session.delete(:user_id)
   end
 
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    User.find_by(id: (session[:user_id]))
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root      'static_pages#index'  
+  root      'static_pages#index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :events, only: [:index, :new, :create]
   resources :users, only: [:new, :create, :show]
-  resources :sessions, only: [:new, :create, :destroy]
+  # resources :sessions, only: [:new, :create, :destroy]
 end
