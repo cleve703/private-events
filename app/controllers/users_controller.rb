701cff974f0e1099ccbdbf8bd8c9dbe47557b9a3
jay <cleve703@gmail.com>
2020-05-14 15:25:08 -0400

    list all attendees on each event show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      redirect_to root_url
     else
       render 'new'
     end
