4e9c3fde07bfd32ce977f052262099cac1b823b0
jay <cleve703@gmail.com>
2020-05-15 14:32:11 -0400

    add scopes to event model for future and previous events

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,6 @@
 class Invitation < ApplicationRecord
   belongs_to :attendee, class_name: "User"
   belongs_to :attended_event, class_name: "Event"
+  validates :attendee, presence: true
+  validates :attended_event, presence: true
 end
